d72a2ecc88dbd34134e940e67d3cb1a5d9e67166
virginiasatyro <vivi_satyro@hotmail.com>
2020-04-08 21:08:03 -0300

    class paddle

--- main.cpp
+++ main.cpp
@@ -104,6 +104,61 @@ public:
 
 };
 
+class Paddle
+{
+private:
+    int _x; // position
+    int _y; 
+    int _x_original; // so we can reset
+    int _y_original;
+
+public:
+    Paddle()
+    {
+        _x = _y = 0;
+    }
+
+    Paddle(int x_pos, int y_pos) : Paddle()
+    {
+        _x_original = x_pos;
+        _y_original = y_pos;
+        _x = x_pos;
+        _y = y_pos;
+    }
+    
+    inline void reset()
+    {
+        _x = _x_original;
+        _y = _y_original;
+    }
+
+    inline int getX()
+    {
+        return _x;
+    }
+
+    inline int getY()
+    {
+        return _y;
+    }
+
+    inline void moveUp()
+    {
+        _y--;
+    }
+
+    inline void moveDown()
+    {
+        _y++;
+    }
+
+    friend ostream & operator << (ostream & o, Paddle paddle)
+    {
+        o << "Paddle [" << paddle._x << "," << paddle._y << "]";
+        return o; 
+    }
+};
+
 int main()
 {
 
@@ -126,6 +181,18 @@ int main()
     ball.move();
 
     cout << "Position after move: " << ball << endl; 
+
+    Paddle player1(0, 0);
+    Paddle player2(10, 0);
+
+    cout << "Player 1: " << player1 << endl;
+    cout << "Player 2: " << player2 << endl;
+
+    player1.moveUp();
+    player2.moveDown();
+
+    cout << "Player 1: " << player1 << endl;
+    cout << "Player 2: " << player2 << endl;
 
     return 0;
 }
