08877d52247dd45415b667a969f786f6515b35e9
virginiasatyro <vivi_satyro@hotmail.com>
2020-04-09 20:10:16 -0300

    playable game

--- main.cpp
+++ main.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
 #include <time.h>
+//#include <curses.h>
 // #include <conio.h>
 
 using namespace std;
+
+// #include <stdio.h>
+// #include <sys/select.h>
+// #include <termios.h>
+// #include <stropts.h>
+
+// int _kbhit() {
+//     static const int STDIN = 0;
+//     static bool initialized = false;
+
+//     if (! initialized) {
+//         // Use termios to turn off line buffering
+//         termios term;
+//         tcgetattr(STDIN, &term);
+//         term.c_lflag &= ~ICANON;
+//         tcsetattr(STDIN, TCSANOW, &term);
+//         setbuf(stdin, NULL);
+//         initialized = true;
+//     }
+
+//     int bytesWaiting;
+//     ioctl(STDIN, FIONREAD, &bytesWaiting);
+//     return bytesWaiting;
+// }
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+ 
+int _kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
 
 enum Direction {STOP = 0, 
                 LEFT = 1, 
@@ -202,7 +259,7 @@ public:
 
     void scoreUp(Paddle *player)
     {
-        if(player == player2)
+        if(player == player1)
             _score1++;
         else if(player == player2)
             _score2++;
@@ -217,7 +274,7 @@ public:
         system("clear"); // cls (windows)
 
         for(int i = 0; i < _width + 2; i++)
-            cout << "#";
+            cout << "▄";
         cout << endl;
 
         for(int i = 0; i < _height; i++)
@@ -232,34 +289,140 @@ public:
                 int player2Y = player2->getY();
 
                 if(j == 0) // first column
-                    cout << "#";
+                   cout << "▋";
 
                 if(ballX == j && ballY == i)
-                    cout << "o";
+                    cout << "●";
+                // player 1 ---------------------------
                 else if(player1X == j && player1Y == i)
-                    cout << "#"; // player 1 
+                    cout << "▋";
+                else if(player1X == j && player1Y + 1 == i)
+                    cout << "▋"; // player 1 
+                else if(player1X == j && player1Y + 2 == i)
+                    cout << "▋"; // player 1 
+                else if(player1X == j && player1Y + 3 == i)
+                    cout << "▋"; // player 1 
+                // --
+
+                // player 2 ---------------------------
                 else if(player2X == j && player2Y == i)
-                    cout << "#"; // player 2
+                    cout << "▐"; // player 2
+                else if(player2X == j && player2Y + 1 == i)
+                    cout << "▐"; // player 2
+                else if(player2X == j && player2Y + 2 == i)
+                    cout << "▐"; // player 2
+                else if(player2X == j && player2Y + 3 == i)
+                    cout << "▐"; // player 2
+                // --
+
                 else
                     cout << " ";
                 
 
                 if(j == _width - 1)
-                    cout << "#";
+                    cout << "▐";
             }
             cout << endl;
         }
 
-        for(int i = 0; i < _width + 2; i++)
-            cout << "#";
+        for(int i = 0; i < _width + 2; i++) 
+            cout << "▀";
         cout << endl;
+
+        // scores 
+        cout << "Score 1 " << _score1 << "\nScore 2 " << _score2 << endl; 
+    }
+
+    void input()
+    {
+        ball->move();
+        
+        int ballX = ball->getX();
+        int ballY = ball->getY();
+        int player1X = player1->getX();
+        int player1Y = player1->getY();
+        int player2X = player2->getX();
+        int player2Y = player2->getY();
+
+        if(_kbhit())
+        {
+            char current = getchar();
+            if(current == move_up1)
+                if(player1Y > 0)
+                    player1->moveUp();
+            if(current == move_up2)
+                if(player1Y > 0)
+                    player2->moveUp();
+            if(current == move_down1)
+                if(player1Y + 4 < _height)
+                    player1->moveDown();
+            if(current == move_down2)
+                if(player2Y + 4 < _height)
+                    player2->moveDown();
+
+            if(ball->getDirection() == STOP)
+                ball->randomDirection();
+
+            if(current == 'q')
+                quit = true;
+        }
+    }
+
+    void logic()
+    {
+        int ballX = ball->getX();
+        int ballY = ball->getY();
+        int player1X = player1->getX();
+        int player1Y = player1->getY();
+        int player2X = player2->getX();
+        int player2Y = player2->getY();
+
+        // left paddle
+        for(int i = 0; i < 4; i++)
+            if(ballX == player1X + 1)
+                if(ballY == player1Y + i)
+                    ball->changeDirection((Direction)(rand()%3 + 4)); // right, upright or downright
+
+        // right paddle
+        for(int i = 0; i < 4; i++)
+        if(ballX == player2X - 1)
+            if(ballY == player2Y + i)
+                ball->changeDirection((Direction)(rand()%3 + 1)); // left, upleft or downleft
+
+        // bottom wall
+        if(ballY == _height - 1) // hit the wall - dont get into it
+            ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+
+        // top wall
+        if(ballY == 0) 
+            ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+        
+        // right wall 
+        if(ballX == _width - 1)
+            scoreUp(player1);
+        
+        // left wall
+        if(ballX == 0)
+            scoreUp(player2); // não está funcionando
+    }
+
+    void run()
+    {
+        while (!quit)
+        {
+            draw();
+            input();
+            logic();
+            usleep(30000);
+        }
+        
     }
 };
 
 int main()
 {
     GameManager game(40, 20);
-    game.draw();
+    game.run();
 
     return 0;
 }
